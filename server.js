664b61e1256c47ce338f96cbac311e37ab799294
Satyam Sakpal <sakpalsatyam3@gmail.com>
2023-08-23 10:24:49 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require('express')
+const path = require('path')
 const sharp = require('sharp')
 const app = express() 
+// app.set ('view engine', 'ejs')
+// app.set('views','src')
+
+app.listen(3000, () => console.log('listening..'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'src/index.html'))
+})
